cdbcfc1840aee783adbe78668d7e09acfa86adc8
cuong8ql3 <cuong8ql3@gmail.com>
2020-04-25 22:34:54 +0700

    training-api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { createStore, applyMiddleware, compose } from 'redux';
+import appReducers from './Reducers/index';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const store = createStore(
+    appReducers,
+    enhancer
+    // compose(applyMiddleware(thunk),window._REDUX_DEVTOOLS_EXTENSION_ && window.__REDUX_DEVTOOLS_EXTENSION__())   
+)
+
+ReactDOM.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>    
+    , document.getElementById('root'));
 serviceWorker.unregister();
